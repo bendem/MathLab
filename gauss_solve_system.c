d42507e99936201e0c098e72f6d581e799100031
bendem <online@bendem.be>
2015-08-21 21:09:51 +0200

    Gaussian method

--- gauss_solve_system.c
+++ gauss_solve_system.c
@@ -33,13 +33,42 @@ void display_equations(double** equations, int count) {
         printf("(%d) ", i);
         for(unsigned j = 0; j < count + 1; ++j) {
             if(j == count) {
-                printf("= ");
+                printf("= %+.1f\n", equations[i][j]);
+                continue;
             }
             printf("%+.1f . x%d\t", equations[i][j], j + 1);
         }
-        printf("\n");
     }
 }
 
 void solve(double** equations, unsigned count) {
+    for(unsigned p = 0; p < count; ++p) {
+        for(unsigned c = p + 1; c < count; ++c) {
+            double m = equations[c][p] / equations[p][p];
+            for(unsigned v = p; v < count + 1; ++v) {
+                equations[c][v] -= equations[p][v] * m;
+            }
+        }
+    }
+
+    printf("\n");
+    display_equations(equations, count);
+
+    double results[count];
+
+    for(unsigned e = count - 1; e != -1; --e) {
+        double s = 0;
+
+        for(unsigned i = count - 1; i > e; --i) {
+            s += results[i] * equations[e][i];
+        }
+
+        results[e] = (equations[e][count] - s) / equations[e][e];
+    }
+
+    printf("\n");
+    for(unsigned i = 0; i < count; ++i) {
+        printf("x%d = %+f\n", i + 1, results[i]);
+    }
+
 }
